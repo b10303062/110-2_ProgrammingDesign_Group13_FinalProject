f14e6406260574090eeec28c3a9581f00a4b97fb
林昱辰 <134303587+b10303062@users.noreply.github.com>
2025-01-24 21:30:08 +0800

    Add files via upload

---
+++ code/ptx.py
@@ -0,0 +1,30 @@
+from hashlib import sha1
+import hmac
+from wsgiref.handlers import format_date_time
+from datetime import datetime
+from time import mktime
+import base64
+from requests import request
+from pprint import pprint
+
+
+class Auth():
+
+    def __init__(self, app_id, app_key):
+        self.app_id = app_id
+        self.app_key = app_key
+
+    def get_auth_header(self):
+        xdate = format_date_time(mktime(datetime.now().timetuple()))
+        hashed = hmac.new(self.app_key.encode('utf8'), ('x-date: ' + xdate).encode('utf8'), sha1)
+        signature = base64.b64encode(hashed.digest()).decode()
+
+        authorization = 'hmac username="' + self.app_id + '", ' + \
+                        'algorithm="hmac-sha1", ' + \
+                        'headers="x-date", ' + \
+                        'signature="' + signature + '"'
+        return {
+            'Authorization': authorization,
+            'x-date': format_date_time(mktime(datetime.now().timetuple())),
+            'x-User-Agent': 'SwaggerUI'
+        }
